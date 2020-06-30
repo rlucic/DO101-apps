5ae8356eff131f55af0341f58544cee451c4a9d6
rgl <romulus.lucic@gmail.com>
2020-06-29 23:46:41 -0400

    added endpoint for generating the png image

--- api-qrcode/src/routers/genericRouter.js
+++ api-qrcode/src/routers/genericRouter.js
@@ -4,6 +4,7 @@
 //import npm libraries
 const express = require('express')
 const moment = require('moment')
+const QRCode = require('qrcode')
 
 //import local libraries
 const sampleAuth = require('../middleware/sampleMiddleware')
@@ -50,6 +51,29 @@ genericRouter.get('/sample', async (req, res) => {
     }
 })
 
+genericRouter.post('/image', sampleAuth, async (req, res) => {
+    const body = req.body
+    console.log(body)
+
+    if (!body.text) {
+        return res.status(400).send({ 'error': 'post a JSON like: text=value' })
+    }
+    var accept = req.accepts(['image/png'])
+    console.log('Accept: ', accept)
+    if (accept === false) {
+        return res.status(406).send('wrong Accept header, Works only with image/png')
+    }
+    try {
+        if (accept.indexOf('image/png') >= 0) {
+            res.setHeader('Content-Type', accept)
+            return await QRCode.toFileStream(res, body.text)
+        }
+        //return res.send('wrong Accept header, Works only with image/png')
+    } catch (error) {
+        console.log(error)
+        return res.status(400).send(error.toString())
+    }
+})
 /**
  * generates a QRCode in an image data or Base64 encoded image
  */
@@ -61,23 +85,26 @@ genericRouter.post('/generate', sampleAuth, async (req, res) => {
         return res.status(400).send({ 'error': 'post a JSON like: text=value' })
     }
     var accept = req.accepts(['text/plain', 'text/html'])
-    console.log(accept)
+    console.log('Accept: ', accept)
     try {
-        if (accept === false){
+        if (accept === false) {
             return res.status(406).send()
         }
-        var img64 = await qrCodeAsDataURI(body.text)
+
         if (accept.indexOf('html') >= 0) {
+            var img64 = await qrCodeAsDataURI(body.text)
             console.log('Accepts text/html')
             console.log(accept.indexOf('html'))
-            res.setHeader('Content-Type', 'text/html')
+            res.setHeader('Content-Type', accept)
             res.send(`<img src=${img64}>`)
         }
+        //console.log('not text/html')
         else if (accept.indexOf('plain') >= 0) {
+            var img64 = await qrCodeAsDataURI(body.text)
             console.log('Accepts text/plain')
             var idx = img64.indexOf('base64')
-            res.setHeader('Content-Type', 'text/plain')
-            return res.send(img64.substring(idx+7))
+            res.setHeader('Content-Type', accept)
+            return res.send(img64.substring(idx + 7))
             //throw new Error('something went wrong')
         }
 
